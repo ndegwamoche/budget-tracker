10cd157f6b56759eeb930660f79f9701c3dfdac6
Martin Moche <151727514+ndegwamoche@users.noreply.github.com>
2026-02-04 15:12:27 +0300

    Hide Firebase config

--- src/config/firebase-config.tsx
+++ src/config/firebase-config.tsx
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6mle9eLLnSp-NtdJY1zpbidG2AXrzGug",
-  authDomain: "budget-tracker-35c4f.firebaseapp.com",
-  projectId: "budget-tracker-35c4f",
-  storageBucket: "budget-tracker-35c4f.firebasestorage.app",
-  messagingSenderId: "548136277782",
-  appId: "1:548136277782:web:279ad889ac0baa7f7e6237",
-  measurementId: "G-59KXBXFCT6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
